21a1b0d4473ad3fafe435a738927ad69523e74e6
wmgaertner <gaertner_william@outlook.com>
2023-08-09 20:27:26 -0400

    fix statusline config after update

--- chadrc.lua
+++ chadrc.lua
@@ -12,21 +12,19 @@ M.ui = {
   transparency = true,
   statusline = {
     theme = "vscode_colored",
-    overriden_modules = function()
-      local st_modules = require "nvchad_ui.statusline.vscode_colored"
-      local function get_npm()
-        local npm = require("package-info").get_status()
-        if npm ~= nil then
-          return npm
-        else
-          return ""
-        end
-      end
-      return {
-        LSP_progress = function()
-          return st_modules.LSP_progress() .. " " .. "%#NpmH1#" .. get_npm()
-        end,
-      }
+    overriden_modules = function(modules)
+      table.insert(
+        modules,
+        6,
+        (function()
+          local package_info = require("package-info").get_status()
+          if package_info ~= nil then
+            return package_info
+          else
+            return ""
+          end
+        end)()
+      )
     end,
   },
 }
